5333af30be66180ddcf95fb51bb4bc304cbb915f
farhanazmiCS <farhanazmi177@gmail.com>
2023-01-24 15:12:17 +0800

    Updated comments in InputManager class

--- core/src/com/mygdx/game/InputManagement.java
+++ core/src/com/mygdx/game/InputManagement.java
@@ -2,4 +2,7 @@ package com.mygdx.game;
 
 public class InputManagement {
     // This class contains the attributes and methods for handling input.
+    // Use cases:
+    //  1. Input for controlling the menu / settings / etc.
+    //  2. Input for controlling in-game entities
 }
